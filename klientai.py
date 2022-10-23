2e72efc0f20544f1b0c44a09144bb43a2d730702
perednioks <peredniokas23@gmail.com>
2022-10-23 12:04:19 +0300

    ikele preke klientui

--- klientai.py
+++ klientai.py
@@ -1,8 +1,19 @@
 from main import session,Product,Customer
 
-customer = Customer(name = "customer 1")
-customer2 = Customer(name = "customer 2")
-customer3 = Customer(name = "customer 3")
+#customer = Customer(name = "customer 1")
+#customer2 = Customer(name = "customer 2")
+#customer3 = Customer(name = "customer 3")
+customer=session.query(Customer).filter(Customer.id==1).first()
+customer2=session.query(Customer).filter(Customer.id==1).first()
 
-session.add_all([customer,customer2,customer3])
+product = Product(name = "bananai", price = 3)
+product2 = Product(name = "apelsinai", price = 2)
+product3 = Product(name = "citrinos", price = 1.5)
+
+customer.products.append(
+    product
+)
+
 session.commit()
+#session.add_all([customer,customer2,customer3])
+#session.commit()
