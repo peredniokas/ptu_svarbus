6548a0163404aa42e7b7b569fb05abea84663eb3
perednioks <peredniokas23@gmail.com>
2022-10-23 09:34:13 +0300

    sujungiu su association table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine,engine
+from sqlalchemy import create_engine, Column, Integer, String, Table, ForeignKey
 
 
 BASE_DIR=os.path.dirname(os.path.realpath(__file__))
@@ -11,6 +11,7 @@ connetion_str='sqlite:///'+os.path.join(BASE_DIR, 'site.sqlite3')
 
 engine=create_engine(connetion_str)
 
+Base=declarative_base()
 """ table association:
     product_id: int fk (products.id)
     customer_id: int fk(customers.id)
@@ -18,10 +19,33 @@ engine=create_engine(connetion_str)
     class klienastas:       
     id                       
     name
-    
+
     class produktai:
     id
     name
     price
-    """
-Base=declarative_base()
+   """
+
+association_table = Table(
+    'association',
+    Base.metadata,
+    Column('customer_id',ForeignKey('customers.id')),
+    Column('product_id', ForeignKey('product.id'))
+)
+
+class Customer(Base):
+    __table__ = 'customers'
+    id = Column(Integer(), primary_key = True)
+    name = Column(String(), nullable= False)
+
+    def __repr__(self):
+        return f"<Customer {self.name}>"
+
+class Product(Base):
+    __table__ = 'products'
+    id = Column(Integer(), primary_key = True)
+    name= Column(String(), nullable = False)
+    price = Column(String(), nullable= False)
+
+    def __repr__(self):
+        return f"<Product {self.name}>"
