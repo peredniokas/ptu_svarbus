2a2e3ad5533b64b07c807320420e849802f93ecf
perednioks <peredniokas23@gmail.com>
2022-10-23 10:45:47 +0300

    sukuria 3 lenteles su sasaja association

--- main.py
+++ main.py
@@ -1,58 +1,46 @@
-import os
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine, Table
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import create_engine, Column, Integer, String, Table, ForeignKey
 from sqlalchemy.orm import relationship
 
-BASE_DIR=os.path.dirname(os.path.realpath(__file__))
 
+engine = create_engine('sqlite:///data/parduotuves.db')
+Base = declarative_base()
 
-connetion_str='sqlite:///'+os.path.join(BASE_DIR, 'site.sqlite3')
-
-
-engine=create_engine(connetion_str)
-
-Base=declarative_base()
-""" table association:
-    product_id: int fk (products.id)
-    customer_id: int fk(customers.id)
-
-    class klienastas:       
-    id                       
-    name
-
-    class produktai:
-    id
-    name
-    price
-   """
-
-association_table = Table(
-    'association',
-    Base.metadata,
-    Column('customer_id',ForeignKey('customers.id')),
-    Column('product_id', ForeignKey('product.id'))
+association_table = Table('association', Base.metadata, 
+    Column('customer_id', Integer, ForeignKey("customers.id")),
+    Column('product_id', Integer, ForeignKey("products.id")),
 )
 
+
 class Customer(Base):
-    __table__ = 'customers'
-    id = Column(Integer(), primary_key = True)
-    name = Column(String(), nullable= False)
-    products = relationship('Product',secondary=association_table
-        back_populates='customers'
+    __tablename__ = "customers"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(), nullable=False)
+    products = relationship("Product", secondary=association_table, 
+        back_populates="customers",
     )
 
     def __repr__(self):
-        return f"<Customer {self.name}>"
+       # return f"({self.id}, {self.name})"
+        return f"Customer {self.name}"
 
 class Product(Base):
-    __table__ = 'products'
-    id = Column(Integer(), primary_key = True)
-    name= Column(String(), nullable = False)
-    price = Column(String(), nullable= False)
+    __tablename__ = "products"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(), nullable=False)
+    price = Column(Integer, nullable=True)
     customers = relationship(
-        'Customer',
-        secondary = association_table,
-        back_populates = 'products'
+        "Customer",
+        secondary=association_table,
+        back_populates="products",
     )
+
     def __repr__(self):
-        return f"<Product {self.name}>"
+        return f"Product {self.name}"
+
+
+if __name__ == "__main__":
+    # Base.metadata.drop_all(engine)
+    Base.metadata.create_all(engine)
+
+    
